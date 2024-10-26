45a5db6a0f5108464f4d32ee2963d27aa1fe98fa
RubenRomeroCab <rubenromerodev@gmail.com>
2024-10-26 06:08:45 +0200

    Terminado

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
@@ -5,10 +6,13 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,],
+  
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
 
+
+  
 }
